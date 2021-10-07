a86c31e0f5f75d6b0252f575bde2e5483a66a0b1
Zelayan <Lyp82nlf>
2021-10-07 21:22:11 +0800

    feat:add  write log

--- write/write.go
+++ write/write.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/snappy"
@@ -12,6 +13,16 @@ import (
 	"github.com/prometheus/prometheus/prompb"
 )
 
+func init() {
+	log.SetPrefix("CZ: ")
+	file, err := os.OpenFile("CZ.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetOutput(file)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
+}
 func main() {
 	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
 		compressed, err := ioutil.ReadAll(r.Body)
@@ -32,18 +43,29 @@ func main() {
 			return
 		}
 
-		for _, ts := range req.Timeseries {
-			m := make(model.Metric, len(ts.Labels))
-			for _, l := range ts.Labels {
-				m[model.LabelName(l.Name)] = model.LabelValue(l.Value)
-			}
-			fmt.Println(m)
+		protoToSamples(&req)
 
-			for _, s := range ts.Samples {
-				fmt.Printf("  %f %d\n", s.Value, s.Timestamp)
-			}
-		}
 	})
 
 	http.ListenAndServe(":1234", nil)
+}
+
+func protoToSamples(req *prompb.WriteRequest) model.Samples {
+	var samples model.Samples
+	for _, ts := range req.Timeseries {
+		metric := make(model.Metric, len(ts.Labels))
+		for _, l := range ts.Labels {
+			metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+		}
+
+		for _, s := range ts.Samples {
+			samples = append(samples, &model.Sample{
+				Metric:    metric,
+				Value:     model.SampleValue(s.Value),
+				Timestamp: model.Time(s.Timestamp),
+			})
+			log.Printf("metric: %s, value: %s, Timestamp: %d", metric, model.SampleValue(s.Value), model.Time(s.Timestamp))
+		}
+	}
+	return samples
 }
